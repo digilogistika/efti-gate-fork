232331731fa9b9ffc1c01226855a52f2cd5f2676
Matti Uusitalo <matti.uusitalo@solita.fi>
2024-11-20 07:47:07 +0200

    EREF-60 Error responses

--- implementation/gate/src/main/java/eu/efti/eftigate/controller/PlatformApiController.java
+++ implementation/gate/src/main/java/eu/efti/eftigate/controller/PlatformApiController.java
@@ -2,6 +2,8 @@ package eu.efti.eftigate.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,12 @@ public class PlatformApiController {
 
     @PutMapping(value = "/v0/consignments/{datasetId}", consumes = "application/xml")
     @Operation(summary = "Add or update consignment identifiers", description = "Adds or updates identifiers for a consignment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<Void> putConsignmentIdentifiers(
             @Parameter(description = "dataset Id", required = true)
             @PathVariable("datasetId") String datasetId,
