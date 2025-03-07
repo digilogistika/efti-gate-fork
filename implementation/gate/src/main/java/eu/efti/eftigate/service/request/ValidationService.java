b8f4b4b18b3508df2c1a51d8c4944d0aef3e2fcd
Matti Uusitalo <47240847+mattiuusitalo@users.noreply.github.com>
2025-03-07 09:16:42 +0200

    Resolve xsds from inside the jar (#67)
    * Resolve xsds from inside the jar
    
    The previous implementation worked for unit tests but not when running the gate as a jar
    
    * Pass exception for easier debugging
    
    * Clarify intent of the code

--- implementation/gate/src/main/java/eu/efti/eftigate/service/request/ValidationService.java
+++ implementation/gate/src/main/java/eu/efti/eftigate/service/request/ValidationService.java
@@ -2,7 +2,7 @@ package eu.efti.eftigate.service.request;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
+import org.w3c.dom.ls.LSInput;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
@@ -11,36 +11,52 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.util.Optional;
 
 @Service
 @Slf4j
 public class ValidationService {
 
-    private static final String GATE_XSD = "classpath:xsd/edelivery/gate.xsd";
+    private static final String GATE_XSD = "xsd/edelivery/gate.xsd";
 
     Validator validator;
 
     public ValidationService() {
         try {
             validator = initValidator();
-        } catch (FileNotFoundException | SAXException e) {
+        } catch (SAXException e) {
             log.error("can't initialize ValidationService", e);
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(e);
         }
     }
 
-    private File getFile() throws FileNotFoundException {
-        return ResourceUtils.getFile(GATE_XSD);
-    }
-
-    private Validator initValidator() throws FileNotFoundException, SAXException {
+    private Validator initValidator() throws SAXException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Source schemaFile = new StreamSource(getFile());
+        factory.setResourceResolver((type, namespaceURI, publicId, systemId, baseURI) -> {
+            if (systemId != null) {
+                String path = systemId;
+                if (systemId.contains("/")) {
+                    path = systemId.substring(systemId.lastIndexOf('/') + 1);
+                }
+
+                // Try different possible locations from inside the JAR
+                for (String prefix : new String[]{"xsd/", "xsd/types/", "xsd/codes/", "xsd/edelivery/"}) {
+                    InputStream stream = getClass().getClassLoader().getResourceAsStream(prefix + path);
+                    if (stream != null) {
+                        LSInput input = new XSDInsideJarInput();
+                        input.setPublicId(publicId);
+                        input.setSystemId(systemId);
+                        input.setBaseURI(baseURI);
+                        input.setByteStream(stream);
+                        input.setEncoding("UTF-8");
+                        return input;
+                    }
+                }
+            }
+            return null;
+        });
+        Source schemaFile = new StreamSource(getClass().getClassLoader().getResourceAsStream(GATE_XSD));
         Schema schema = factory.newSchema(schemaFile);
         return schema.newValidator();
     }
@@ -53,6 +69,98 @@ public class ValidationService {
             return Optional.of(e.getMessage());
         }
         return Optional.empty();
+    }
+
+    private static class XSDInsideJarInput implements LSInput {
+
+        private Reader characterStream;
+        private InputStream byteStream;
+        private String stringData;
+        private String systemId;
+        private String publicId;
+        private String baseURI;
+        private String encoding;
+        private boolean certifiedText;
+
+        @Override
+        public Reader getCharacterStream() {
+            return characterStream;
+        }
+
+        @Override
+        public void setCharacterStream(Reader characterStream) {
+            this.characterStream = characterStream;
+        }
+
+        @Override
+        public InputStream getByteStream() {
+            return byteStream;
+        }
+
+        @Override
+        public void setByteStream(InputStream byteStream) {
+            this.byteStream = byteStream;
+        }
+
+        @Override
+        public String getStringData() {
+            return stringData;
+        }
+
+        @Override
+        public void setStringData(String stringData) {
+            this.stringData = stringData;
+        }
+
+        @Override
+        public String getSystemId() {
+            return systemId;
+        }
+
+        @Override
+        public void setSystemId(String systemId) {
+            this.systemId = systemId;
+        }
+
+        @Override
+        public String getPublicId() {
+            return publicId;
+        }
+
+        @Override
+        public void setPublicId(String publicId) {
+            this.publicId = publicId;
+        }
+
+        @Override
+        public String getBaseURI() {
+            return baseURI;
+        }
+
+        @Override
+        public void setBaseURI(String baseURI) {
+            this.baseURI = baseURI;
+        }
+
+        @Override
+        public String getEncoding() {
+            return encoding;
+        }
+
+        @Override
+        public void setEncoding(String encoding) {
+            this.encoding = encoding;
+        }
+
+        @Override
+        public boolean getCertifiedText() {
+            return certifiedText;
+        }
+
+        @Override
+        public void setCertifiedText(boolean certifiedText) {
+            this.certifiedText = certifiedText;
+        }
     }
 
 }
