96201c4b6ef0253d86996ad3230d0a7591b9dcbb
Rasmus <rraasu@taltech.ee>
2025-04-19 21:17:22 +0300

    feat: Removed everything from the deploy folder

--- deploy/local/domibus/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-cd $(dirname $0)
-
-echo "Starting up docker compose"
-docker compose up -d
-
-$SHELL
--- deploy/prod/server-2/gate/sql/1-create_tables.sql
+++
@@ -1,3 +0,0 @@
--- create user
-create user efti with encrypted password 'root';
-grant all privileges on database efti to efti;
