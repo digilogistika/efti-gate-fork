aa122da3c7f4ca7abd1c5eef41d7fb5f5d325374
Rihard Rivis <rihard.rivis@proton.me>
2025-05-08 18:36:23 +0300

    doc: api that is on the platform

--- implementation/platform-gate-simulator/src/main/java/eu/efti/platformgatesimulator/controller/GateApiController.java
+++ implementation/platform-gate-simulator/src/main/java/eu/efti/platformgatesimulator/controller/GateApiController.java
@@ -1,6 +1,7 @@
 package eu.efti.platformgatesimulator.controller;
 
 import eu.efti.platformgatesimulator.config.GateProperties;
+import eu.efti.platformgatesimulator.controller.api.GateApi;
 import eu.efti.platformgatesimulator.service.IdentifierService;
 import eu.efti.platformgatesimulator.service.ReaderService;
 import eu.efti.v1.consignment.common.SupplyChainConsignment;
@@ -8,7 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
@@ -18,20 +22,18 @@ import java.util.Set;
 @RequestMapping("/gate-api")
 @AllArgsConstructor
 @Slf4j
-public class GateApiController {
+public class GateApiController implements GateApi {
     private final ReaderService readerService;
     private final GateProperties gateProperties;
     private final IdentifierService identifierService;
 
-    @GetMapping(
-            path = "/consignments",
-            produces = MediaType.APPLICATION_XML_VALUE
-    )
+
     public ResponseEntity<Object> getConsignmentSubsets(
             @RequestParam String datasetId,
             @RequestParam Set<String> subsetId,
             @RequestParam String requestId
     ) {
+        log.info("GET on /gate-api/consignments with params datasetId: {}, subsetId: {}, requestId: {}", datasetId, subsetId, requestId);
         try {
             SupplyChainConsignment supplyChainConsignment = readerService.readFromFile(gateProperties.getCdaPath() + datasetId, subsetId.stream().toList());
             String uilResponse = identifierService.buildBody(requestId, supplyChainConsignment);
@@ -56,10 +58,6 @@ public class GateApiController {
         }
     }
 
-    @PostMapping(
-            path = "/follow-up",
-            consumes = MediaType.APPLICATION_XML_VALUE
-    )
     public ResponseEntity<Void> postConsignmentFollowup(@RequestBody String body) {
         log.info("POST on /follow-up with body {}", body);
         return null;
