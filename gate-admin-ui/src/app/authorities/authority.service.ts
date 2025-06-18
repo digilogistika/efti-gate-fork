c61d4f666b542f677ecf70e6bf2734d8509727df
Rasmus <rraasu@taltech.ee>
2025-06-18 12:16:44 +0300

    feat: Added functionality to Authority Registration.

---
+++ gate-admin-ui/src/app/authorities/authority.service.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from "@angular/core";
+import { environment } from "../../environments/environment.development";
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { ApiKeyResponse } from "../authentication/api-key-response.model";
+import { Observable } from "rxjs";
+import { Authority } from "./authority.model";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthorityService {
+  baseUrl = environment.baseUrl;
+  registerUrl = this.baseUrl + environment.apiUrl.registerAuthority;
+
+  private readonly http = inject(HttpClient);
+
+  register(authority: Authority): Observable<HttpResponse<ApiKeyResponse>> {
+    return this.http.post<ApiKeyResponse>(this.registerUrl, authority, {
+      observe: "response",
+    });
+  }
+}
--- gate-admin-ui/src/app/platforms/platform-register-response.model.ts
+++
@@ -1,3 +0,0 @@
-export interface PlatformRegisterResponse {
-  apiKey: string
-}
