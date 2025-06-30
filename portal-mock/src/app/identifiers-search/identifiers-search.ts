622def02fa6f0b4cacd7b2d9f005116e11a0b8a3
Rihard Rivis <rihard.rivis@proton.me>
2025-06-30 16:21:21 +0300

    feat: identifiers search

--- portal-mock/src/app/identifiers-search/identifiers-search.ts
+++ portal-mock/src/app/identifiers-search/identifiers-search.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {euCountries} from './countries';
+import {MatFormField, MatLabel, MatOption, MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-identifiers-search',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelect,
+    MatLabel,
+    MatFormField,
+    MatOption,
+  ],
   templateUrl: './identifiers-search.html',
 })
 export class IdentifiersSearch {
+  public readonly identifiersSearchForm: FormGroup;
+  protected readonly euCountries;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.identifiersSearchForm = this.fb.group({
+      identifier: ['', [Validators.required]],
+      modeCode: [''],
+      identifierType: [[]],
+      registrationCountryCode: [''],
+      dangerousGoodsIndicator: [''],
+      eftiGateIndicator: [[]]
+    });
+    this.euCountries = euCountries;
+  }
+
+  onSubmit() {
+    if (!this.identifiersSearchForm.valid) {
+      return
+    }
+    console.log(this.identifiersSearchForm.value)
+  }
 }
