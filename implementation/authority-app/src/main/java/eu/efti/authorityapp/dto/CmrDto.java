1965cc01163fae9df6f1dd1f162513f340d78781
kaspar <kaslep@taltech.ee>
2025-07-24 15:45:45 +0300

    Removed xml viewer and added more data to pdf

--- implementation/authority-app/src/main/java/eu/efti/authorityapp/dto/CmrDto.java
+++ implementation/authority-app/src/main/java/eu/efti/authorityapp/dto/CmrDto.java
@@ -8,6 +8,7 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,27 +16,85 @@ import lombok.Setter;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CmrDto {
 
+    // Box 1: Corresponds to "Sender" in the report
     @XmlElement(name = "consignor", namespace = "http://efti.eu/v1/consignment/common")
-    private Consignor consignor;
+    private Party consignor;
 
+    // Box 2: Consignee
     @XmlElement(name = "consignee", namespace = "http://efti.eu/v1/consignment/common")
-    private Consignee consignee;
+    private Party consignee;
+
+    // Box 3: Taking over the Goods
+    @XmlElement(name = "carrierAcceptanceLocation", namespace = "http://efti.eu/v1/consignment/common")
+    private Location carrierAcceptanceLocation;
+
+    @XmlElement(name = "carrierAcceptanceDateTime", namespace = "http://efti.eu/v1/consignment/common")
+    private String carrierAcceptanceDateTime;
+
+    // Box 4: Delivery of the Goods
+    @XmlElement(name = "consigneeReceiptLocation", namespace = "http://efti.eu/v1/consignment/common")
+    private Location consigneeReceiptLocation;
+
+    // Box 5: Sender's instructions
+    @XmlElement(name = "consignorProvidedInformationText", namespace = "http://efti.eu/v1/consignment/common")
+    private String sendersInstructions;
+
+    // Box 6: Carrier
+    @XmlElement(name = "carrier", namespace = "http://efti.eu/v1/consignment/common")
+    private Party carrier;
+
+    // Box 6: Carrier License Plate
+    @XmlElement(name = "mainCarriageTransportMovement", namespace = "http://efti.eu/v1/consignment/common")
+    private MainCarriageTransportMovement mainCarriageTransportMovement;
+
+    // Box 7: Successive Carriers
+    @XmlElement(name = "connectingCarrier", namespace = "http://efti.eu/v1/consignment/common")
+    private Party successiveCarrier;
+
+    // Box 8: Carrier's reservations and observations
+    @XmlElement(name = "information", namespace = "http://efti.eu/v1/consignment/common")
+    private String carrierReservationsObservations;
+
+    // Box 9: Documents handed to the carrier
+    @XmlElement(name = "associatedDocument", namespace = "http://efti.eu/v1/consignment/common")
+    private AssociatedDocument documentsRemarks;
+
+    // Box 10-15: Consignment Item Details
+    @XmlElement(name = "transportPackage", namespace = "http://efti.eu/v1/consignment/common")
+    private List<TransportPackage> transportPackages;
+
+    @XmlElement(name = "natureIdentificationCargo", namespace = "http://efti.eu/v1/consignment/common")
+    private NatureIdentificationCargo natureIdentificationCargo;
+
+    @XmlElement(name = "grossWeight", namespace = "http://efti.eu/v1/consignment/common")
+    private Measure grossWeight;
+
+    @XmlElement(name = "grossVolume", namespace = "http://efti.eu/v1/consignment/common")
+    private Measure grossVolume;
+
+    // Box 16: Special agreements
+    @XmlElement(name = "contractTermsText", namespace = "http://efti.eu/v1/consignment/common")
+    private String customSpecialAgreement;
+
+    // Box 18: Other useful particulars
+    @XmlElement(name = "deliveryInformation", namespace = "http://efti.eu/v1/consignment/common")
+    private String customParticulars;
+
+    // Box 19: Cash on delivery
+    @XmlElement(name = "cODAmount", namespace = "http://efti.eu/v1/consignment/common")
+    private Amount customCashOnDelivery;
+
+    // Box 21: Established In / Date
+    @XmlElement(name = "transportContractDocument", namespace = "http://efti.eu/v1/consignment/common")
+    private TransportContractDocument transportContractDocument;
+
+
+    // --- Nested classes for JAXB mapping ---
 
     @Getter
     @Setter
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Consignor {
-        @XmlElement(name = "name", namespace = "http://efti.eu/v1/consignment/common")
-        private String name;
-
-        @XmlElement(name = "postalAddress", namespace = "http://efti.eu/v1/consignment/common")
-        private PostalAddress postalAddress;
-    }
-
-    @Getter
-    @Setter
-    @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Consignee {
+    public static class Party {
         @XmlElement(name = "name", namespace = "http://efti.eu/v1/consignment/common")
         private String name;
 
@@ -58,5 +117,85 @@ public class CmrDto {
 
         @XmlElement(name = "countryCode", namespace = "http://efti.eu/v1/consignment/common")
         private String countryCode;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Location {
+        @XmlElement(name = "name", namespace = "http://efti.eu/v1/consignment/common")
+        private String name;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class MainCarriageTransportMovement {
+        @XmlElement(name = "usedTransportMeans", namespace = "http://efti.eu/v1/consignment/common")
+        private UsedTransportMeans usedTransportMeans;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class UsedTransportMeans {
+        @XmlElement(name = "id", namespace = "http://efti.eu/v1/consignment/common")
+        private String id;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class AssociatedDocument {
+        @XmlElement(name = "id", namespace = "http://efti.eu/v1/consignment/common")
+        private String id;
+        @XmlElement(name = "typeCode", namespace = "http://efti.eu/v1/consignment/common")
+        private String typeCode;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class TransportPackage {
+        @XmlElement(name = "itemQuantity", namespace = "http://efti.eu/v1/consignment/common")
+        private Integer itemQuantity;
+        @XmlElement(name = "typeCode", namespace = "http://efti.eu/v1/consignment/common")
+        private String typeCode;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class NatureIdentificationCargo {
+        @XmlElement(name = "identificationText", namespace = "http://efti.eu/v1/consignment/common")
+        private String identificationText;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Measure {
+        @jakarta.xml.bind.annotation.XmlAttribute(name = "unitId")
+        private String unitId;
+        @jakarta.xml.bind.annotation.XmlValue
+        private java.math.BigDecimal value;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Amount {
+        @jakarta.xml.bind.annotation.XmlAttribute(name = "currencyId")
+        private String currencyId;
+        @jakarta.xml.bind.annotation.XmlValue
+        private java.math.BigDecimal value;
+    }
+
+    @Getter
+    @Setter
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class TransportContractDocument {
+        @XmlElement(name = "id", namespace = "http://efti.eu/v1/consignment/common")
+        private String id;
     }
 }
--- implementation/authority-app/src/main/java/eu/efti/authorityapp/service/PdfGenerationService.java
+++ implementation/authority-app/src/main/java/eu/efti/authorityapp/service/PdfGenerationService.java
@@ -9,13 +9,17 @@ import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -40,34 +44,115 @@ public class PdfGenerationService {
             final JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
             log.info("Report compiled successfully.");
 
-            // Step 3: Populate the parameters map from the CmrDto
             final Map<String, Object> parameters = new HashMap<>();
 
-            if (cmrDto.getConsignor() != null) {
-                parameters.put("consignorName", cmrDto.getConsignor().getName());
-                if (cmrDto.getConsignor().getPostalAddress() != null) {
-                    CmrDto.PostalAddress address = cmrDto.getConsignor().getPostalAddress();
-                    parameters.put("consignorStreet", address.getStreetName());
-                    parameters.put("consignorCity", address.getCityName());
-                    parameters.put("consignorPostcode", address.getPostcode());
-                    parameters.put("consignorCountry", address.getCountryCode());
-                }
-            }
+            // Box 1: Sender (Consignor)
+            Optional.ofNullable(cmrDto.getConsignor()).ifPresent(consignor -> {
+                parameters.put("senderCompanyName", consignor.getName());
+                Optional.ofNullable(consignor.getPostalAddress()).ifPresent(address -> {
+                    parameters.put("senderStreet", address.getStreetName());
+                    parameters.put("senderCity", address.getCityName());
+                    parameters.put("senderPostCode", address.getPostcode());
+                    parameters.put("senderCountry", address.getCountryCode());
+                });
+            });
 
-            if (cmrDto.getConsignee() != null) {
-                parameters.put("consigneeName", cmrDto.getConsignee().getName());
-                if (cmrDto.getConsignor().getPostalAddress() != null) {
-                    CmrDto.PostalAddress address = cmrDto.getConsignor().getPostalAddress();
+            // Box 2: Consignee
+            Optional.ofNullable(cmrDto.getConsignee()).ifPresent(consignee -> {
+                parameters.put("consigneeCompanyName", consignee.getName());
+                // BUG FIX: Was using consignor's address, now correctly uses consignee's.
+                Optional.ofNullable(consignee.getPostalAddress()).ifPresent(address -> {
                     parameters.put("consigneeStreet", address.getStreetName());
                     parameters.put("consigneeCity", address.getCityName());
                     parameters.put("consigneePostcode", address.getPostcode());
-                    parameters.put("consigneeCountry", address.getCountryCode());
-                }
-            }
+                    // BUG FIX: JRXML parameter is "consigneeCountryCode" not "consigneeCountry"
+                    parameters.put("consigneeCountryCode", address.getCountryCode());
+                });
+            });
+
+//            // Box 3: Taking over the Goods
+//            Optional.ofNullable(cmrDto.getCarrierAcceptanceLocation())
+//                    .ifPresent(loc -> parameters.put("takingOverTheGoodsPlace", loc.getName()));
+//            // Note: The XML has one 'carrierAcceptanceDateTime'. We map it to 'Arrival'. 'Departure' is not in the XML.
+//            parameters.put("logisticsTimeOfArrivalDateTime", cmrDto.getCarrierAcceptanceDateTime());
+//
+            // Box 4: Delivery of the Goods
+            Optional.ofNullable(cmrDto.getConsigneeReceiptLocation())
+                    .ifPresent(loc -> parameters.put("deliveryOfTheGoodsPlace", loc.getName()));
+//
+//            // Box 5: Sender's instructions
+//            parameters.put("sendersInstructions", cmrDto.getSendersInstructions());
+//
+            // Box 6: Carrier
+            Optional.ofNullable(cmrDto.getCarrier()).ifPresent(carrier -> {
+                parameters.put("carrierCompanyName", carrier.getName());
+                Optional.ofNullable(carrier.getPostalAddress()).ifPresent(address -> {
+                    parameters.put("carrierStreet", address.getStreetName());
+                    parameters.put("carrierCity", address.getCityName());
+                    parameters.put("carrierPostcode", address.getPostcode());
+                    parameters.put("carrierCountry", address.getCountryCode());
+                });
+            });
+//            Optional.ofNullable(cmrDto.getMainCarriageTransportMovement())
+//                    .map(CmrDto.MainCarriageTransportMovement::getUsedTransportMeans)
+//                    .ifPresent(utm -> parameters.put("carrierLicensePlate", utm.getId()));
+//
+//
+//            // Box 8: Carrier's reservations and observations
+//            parameters.put("carrierReservationsObservations", cmrDto.getCarrierReservationsObservations());
+//
+//            // Box 9: Documents Remarks
+//            Optional.ofNullable(cmrDto.getDocumentsRemarks())
+//                    .ifPresent(doc -> {
+//                        String remark = "Type: " + doc.getTypeCode() + ", ID: " + doc.getId();
+//                        parameters.put("documentsRemarks", remark);
+//                    });
+//
+            // Box 16: Special agreements
+            parameters.put("customSpecialAgreement", cmrDto.getCustomSpecialAgreement());
+//
+//            // Box 18: Other useful particulars
+//            parameters.put("customParticulars", cmrDto.getCustomParticulars());
+//
+//            // Box 19: Cash on delivery
+//            Optional.ofNullable(cmrDto.getCustomCashOnDelivery())
+//                    .ifPresent(cod -> parameters.put("customCashOnDelivery", cod.getValue()));
+//
+//            // Box 21: Established In / Date
+//            // The XML does not provide a clear source for "Established In" city or the date.
+//            // We are using the transportContractDocument ID as a placeholder to show data flow.
+//            Optional.ofNullable(cmrDto.getTransportContractDocument())
+//                    .ifPresent(tcd -> parameters.put("customEstablishedIn", "Ref Doc: " + tcd.getId()));
+//
+//            // Handle the item table (Dataset)
+//            // The XML data is flat, not a list of items as the JRXML expects.
+//            // We will construct a "best-effort" single-item list from the top-level consignment data.
+//            final List<Map<String, Object>> items = new ArrayList<>();
+//            final Map<String, Object> item = new HashMap<>();
+
+//            if (cmrDto.getTransportPackages() != null && !cmrDto.getTransportPackages().isEmpty()) {
+//                CmrDto.TransportPackage firstPackage = cmrDto.getTransportPackages().get(0);
+//                item.put("logisticsPackageItemQuantity", firstPackage.getItemQuantity());
+//                item.put("logisticsPackageType", firstPackage.getTypeCode());
+//            }
+//
+//            Optional.ofNullable(cmrDto.getNatureIdentificationCargo())
+//                    .ifPresent(cargo -> item.put("transportCargoIdentification", cargo.getIdentificationText()));
+//
+//            Optional.ofNullable(cmrDto.getGrossWeight())
+//                    .ifPresent(weight -> item.put("supplyChainConsignmentItemGrossWeight", weight.getValue()));
+//
+//            Optional.ofNullable(cmrDto.getGrossVolume())
+//                    .ifPresent(volume -> item.put("supplyChainConsignmentItemGrossVolume", volume.getValue()));
+//
+//
+//            items.add(item);
+//            parameters.put("items", new JRBeanCollectionDataSource(items));
+
 
             log.info("Populated report parameters.");
 
-            // Step 4: Fill the report using the parameters and an EMPTY data source
+            // Step 4: Fill the report
             log.info("Filling the report with data...");
             final byte[] pdfBytes = JasperRunManager.runReportToPdf(jasperReport, parameters, new JREmptyDataSource());
             log.info("Report filled and exported to PDF successfully.");
